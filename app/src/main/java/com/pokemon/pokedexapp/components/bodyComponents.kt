ed1946783438bc54abc5952c49b68d44ae12178c
Jose David Falcon Sosa <90795629+JDxy@users.noreply.github.com>
2024-02-21 17:51:59 +0000

    Cree los componentes del cuerpo

---
+++ app/src/main/java/com/pokemon/pokedexapp/components/bodyComponents.kt
@@ -0,0 +1,24 @@
+package com.pokemon.pokedexapp.components
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+// Función componible para agregar espacio vertical
+@Composable
+fun SpacerV(size: Dp = 500.dp){
+    Spacer(modifier = Modifier.height(size))
+}
+
+// Función componible para agregar espacio horizontal
+@Composable
+fun SpacerH(size: Dp = 500.dp){
+    Spacer(modifier = Modifier.width(size))
+}
